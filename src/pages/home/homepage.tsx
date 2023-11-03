a16d6552da082ccee7d0c95fa247d608287e0cfd
Yippei <v.886fgla886@icloud.com>
2023-11-04 00:46:50 +0900

    フォルダーの新規追加とホームページ一部追加

---
+++ src/pages/home/homepage.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+
+//画面設計コード
+const Home: NextPage = () => {
+  return (
+    <p>
+      <div><big>俺のポートフォリオ</big></div>
+      <div>今できること</div>
+      <div>保持資格</div>
+      <div>SNS一覧</div>
+    </p>
+  )
+}
+
+export default Home
